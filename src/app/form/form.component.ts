4dfc522dad0b9dbb65552ba21f0a73e8f94d0a57
pavantamidala <pavantamidala023@gmail.com>
2021-06-01 14:40:45 +0530

    completed the remaining tasks

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,59 +1,100 @@
 import { Component, OnInit } from '@angular/core';
 
 interface User {
-  firstname: string;
-  lastname: string;
+  firstName: string;
+  lastName: string;
   number: number;
   email: string;
+  // profileImage:any;
 }
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
-
-  constructor() { }
-  public allUsers: any = []
+export class FormComponent {
+  constructor() {}
+  public allUsers: any = [];
+  firstNameFieldValidator:Boolean = false
+   lastNameFieldValidator:Boolean = false
+   numberFieldValidator:Boolean = false
   user: User = {
-    firstname: "",
-    lastname: "",
+    firstName: '',
+    lastName: '',
     number: null,
-    email: ""
-  }
- 
-   date = new Date()
+    email: '',
+    // profileImage:null,
+  };
+
+  date = new Date();
   clickHandle(e) {
-    
-    this.allUsers.push({ firstname: this.user.firstname, lastname: this.user.lastname, number: this.user.number, email: this.user.email,id:this.date.getMilliseconds()+Math.random()})
-    
-    console.log(this.allUsers)
-    this.user.firstname = ""
-    this.user.lastname = ""
-    this.user.number = null
-    this.user.email = ""
-  }
-  deleteHandle(e,id){
-    console.log(id)
-     this.allUsers = this.allUsers.filter((obj)=>{
-      return obj.id !== id
-    })
-  }
-  editHandle(e,id){
+    console.log(e)
     debugger
-  console.log(id)
+    this.firstNameValidator(e.firstName)
+    this.lastNameValidator(e.lastName)
+    this.numberValidator(e.number)
+    if(!this.firstNameFieldValidator && !this.lastNameFieldValidator && !this.numberFieldValidator){
+      this.allUsers.push(e);
+      this.user.firstName = '';
+      this.user.lastName = '';
+      this.user.number = null;
+      this.user.email = '';
+      // this.user.profileImage = null
+    }
+    
+  }
+  deleteHandle(id) {
+    console.log(id);
+     let response =  confirm("Do you want to delete the post?")
+     if(response){
+       this.allUsers = this.allUsers.filter((obj) => {
+         return obj.id !== id;
+       })
+     }
+  }
+  editHandle(id) {
+    
+    console.log(id);
     let temp = this.allUsers.find((obj) => {
-      return obj.id === id
-    })
-    this.user.firstname = temp.firstname
-    this.user.lastname = temp.lastname
-    this.user.number = temp.number
-    this.user.email = temp.email
+      return obj.id === id;
+    });
+    this.user.firstName = temp.firstName;
+    this.user.lastName = temp.lastName;
+    this.user.number = temp.number;
+    this.user.email = temp.email;
     this.allUsers = this.allUsers.filter((obj) => {
-      return obj.id !== id
-    })
+      return obj.id !== id;
+    });
   }
-  ngOnInit(): void {
+  firstNameValidator(str){
+    
+  if(str.length===0){
+    this.firstNameFieldValidator = true;
+    return;
+  }else{
+    this.firstNameFieldValidator = false;
+   return;
   }
+}
+lastNameValidator(str){
+  if(str.length===0){
+    this.lastNameFieldValidator = true;
+    return;
+  }else{
+    this.lastNameFieldValidator = false;
+   return;
+  }
+}
+
+numberValidator(num){
+  if(String(num).length !== 10 ){
+    this.numberFieldValidator = true
+    return;
+  }else{
+    this.numberFieldValidator = false;
+ return;
+  }
+}
 
 }
+
