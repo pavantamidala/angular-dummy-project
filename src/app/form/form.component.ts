ce777855698e3380c52018a92fe3cc4e916ca05a
pavantamidala <pavantamidala023@gmail.com>
2021-06-02 10:39:17 +0530

    added angular validations

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,61 +3,73 @@ import { Component, OnInit } from '@angular/core';
 interface User {
   firstName: string;
   lastName: string;
-  number: number;
+  number: string;
   email: string;
   // profileImage:any;
 }
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css'],
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
   constructor() {}
-  public allUsers: any = [];
-  firstNameFieldValidator:Boolean = false
-   lastNameFieldValidator:Boolean = false
-   numberFieldValidator:Boolean = false
+  allUsers: any = [];
+  firstNameFieldValidator: Boolean = false;
+  lastNameFieldValidator: Boolean = false;
+  numberFieldValidator: Boolean = false;
   user: User = {
     firstName: '',
     lastName: '',
     number: null,
     email: '',
-    // profileImage:null,
+    
   };
 
   date = new Date();
-  clickHandle(e) {
-    console.log(e)
-    debugger
-    this.firstNameValidator(e.firstName)
-    this.lastNameValidator(e.lastName)
-    this.numberValidator(e.number)
-    if(!this.firstNameFieldValidator && !this.lastNameFieldValidator && !this.numberFieldValidator){
-      e.id=this.date.getMilliseconds()+Math.random()
-      this.allUsers.push(e);
-      this.user.firstName = '';
-      this.user.lastName = '';
-      this.user.number = null;
-      this.user.email = '';
-      // this.user.profileImage = null
+  clickHandle(form) {
+    console.log(form);
+    if(form.valid ){
+      form.value.id = this.date.getMilliseconds() + Math.random();
+      this.allUsers.push(form.value);
+      this.cleaningForm();
+      localStorage.setItem("allUsersArray",JSON.stringify(this.allUsers))
+      this.storingInLocalStorage()
     }
+  }
+  storingInLocalStorage(){  
+    const response =  localStorage.getItem("allUsersArray")
+    if(response === null){
+      localStorage.setItem("allUsersArray",JSON.stringify(this.allUsers))
+    }else{
+      const response = localStorage.getItem("allUsersArray")
+      this.allUsers = JSON.parse(response)
+    }
+  }
+ ngOnInit(){
+   this.storingInLocalStorage()
+ }
+  cleaningForm() {
+    this.user.firstName = '';
+    this.user.lastName = '';
+    this.user.number = null;
+    this.user.email = '';
     
   }
   deleteHandle(id) {
-    debugger
-    console.log(id);
-     let response =  confirm("Do you want to delete the post?")
-     if(response){
-       this.allUsers = this.allUsers.filter((obj) => {
-         return obj.id !== id;
-       })
-     }
-  }
-  editHandle(id) {
     
     console.log(id);
-    let editedUsers = this.allUsers.find((obj) => {
+    const response = confirm('Do you want to delete the post?');
+    if (response) {
+      this.allUsers = this.allUsers.filter((obj) => {
+        return obj.id !== id;
+      });
+      localStorage.setItem('allUsersArray', JSON.stringify(this.allUsers));
+    }
+  }
+  editHandle(id) {
+    console.log(id);
+    const editedUsers = this.allUsers.find((obj) => {
       return obj.id === id;
     });
     this.user.firstName = editedUsers.firstName;
@@ -68,35 +80,5 @@ export class FormComponent {
       return obj.id !== id;
     });
   }
-  firstNameValidator(str){
-    
-  if(str.length===0){
-    this.firstNameFieldValidator = true;
-    return;
-  }else{
-    this.firstNameFieldValidator = false;
-   return;
-  }
+  
 }
-lastNameValidator(str){
-  if(str.length===0){
-    this.lastNameFieldValidator = true;
-    return;
-  }else{
-    this.lastNameFieldValidator = false;
-   return;
-  }
-}
-
-numberValidator(num){
-  if(String(num).length !== 10 ){
-    this.numberFieldValidator = true
-    return;
-  }else{
-    this.numberFieldValidator = false;
- return;
-  }
-}
-
-}
-
